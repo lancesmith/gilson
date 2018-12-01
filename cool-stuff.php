2794d39a7f8c782bad089a85d2031c938ba6d689
Lance <lance.smith@echoorigin.com>
2018-12-01 13:26:43 -0500

    cool stuff main page

--- cool-stuff.php
+++ cool-stuff.php
@@ -7,6 +7,19 @@
   <div class="content-wrapper">
   	<div class="inner">
   		<div class="full-width">
+  		<div class="cool-stuff-wrapper">
+  			<ul>
+  			<li><a href=""><img src="images/cool-stuff/thumbs/tedtalk-thumb.png"><h6>Gilson Startup Story, told by Nick Gilson at TEDx 2017</h6></a></li>
+  			<li><a href=""><img src="images/cool-stuff/thumbs/rawlings-thumb.png"><h6>Rawlings Collab</h6></a></li>
+  			<li><a href=""><img src="images/cool-stuff/thumbs/thunderstruck-thumb.png"><h6>Thunderstruck Collab</h6></a></li>
+  			<li><a href=""><img src="images/cool-stuff/thumbs/tedtalk-thumb.png"><h6>Gilson Startup Story, told by Nick Gilson at TEDx 2017</h6></a></li>
+  			<li><a href=""><img src="images/cool-stuff/thumbs/rawlings-thumb.png"><h6>Rawlings Collab</h6></a></li>
+  			<li><a href=""><img src="images/cool-stuff/thumbs/thunderstruck-thumb.png"><h6>Thunderstruck Collab</h6></a></li>
+  			<li><a href=""><img src="images/cool-stuff/thumbs/tedtalk-thumb.png"><h6>Gilson Startup Story, told by Nick Gilson at TEDx 2017</h6></a></li>
+  			<li><a href=""><img src="images/cool-stuff/thumbs/rawlings-thumb.png"><h6>Rawlings Collab</h6></a></li>
+  			<li><a href=""><img src="images/cool-stuff/thumbs/thunderstruck-thumb.png"><h6>Thunderstruck Collab</h6></a></li>
+  			</ul>
+  			</div>
 <?php include 'button-set.php' ?>
     </div>
     </div>
