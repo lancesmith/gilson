a8575a63e783d5f1ffb1b303d25a88c9f92c91f1
Lance <lance.smith@echoorigin.com>
2018-12-05 15:31:40 -0500

    option for images instead of video for cool stuff

---
+++ cool-stuff-entry-image.php
@@ -0,0 +1,24 @@
+<?php include 'head.php' ?>
+<?php include 'header.php' ?>
+  <div class="main-content secondary">
+  <div class="hero cool-stuff">
+  		<h1>COOL STUFF</h1>
+  	</div>
+  <div class="content-wrapper item-page cool-stuff image">
+  <div class="item-wrapper section-2">
+    <div class="video-description">
+    <div class="cool-stuff image">
+    <img src="images/cool-stuff/thumbs/tedtalk-thumb.png">
+    </div>
+    <div class="description">
+    <h3>Gilson x Knoebels - Phoenix Board
+    </h3>
+    <h4>Subhead with short description</h4>
+    <p>For those of you who don't know about Knoebels, they are the super fun amusement park down the road from us here in PA.  They are also home to some of the coolest rides around, including the #1 ranked Wooden Roller Coaster in the world - the Phoenix.  Rising from the ashes of its history as the Rocket roller coaster built in 1947 in San Antonio Texas, the Phoenix was reborn at Knoebels in 1985. In a collaboration with Knoebels, we built 4 snowboards out of actualy wooden planks from the orignal roller coaster!  Check out the journey here!</p>
+    </div>
+    </div>
+<?php include 'button-set.php' ?>
+    </div>
+    </div>
+    </div>
+<?php include 'footer.php' ?>
