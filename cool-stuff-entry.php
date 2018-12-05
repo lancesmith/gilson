ffcd028ab522c714d8f58075c021d267b76574a6
Lance <lance.smith@echoorigin.com>
2018-12-04 21:24:20 -0500

    new cool stuff assets

---
+++ cool-stuff-entry.php
@@ -0,0 +1,24 @@
+<?php include 'head.php' ?>
+<?php include 'header.php' ?>
+  <div class="main-content secondary">
+  <div class="hero cool-stuff">
+  		<h1>COOL STUFF</h1>
+  	</div>
+  <div class="content-wrapper item-page cool-stuff">
+  <div class="item-wrapper section-2">
+    <div class="video-description">
+    <div class="cool-stuff video">
+    <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FGilsonSnow%2Fvideos%2F1782494225120114%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe></div>
+    </div>
+    <div class="description">
+    <h3>Gilson x Knoebels - Phoenix Board
+    </h3>
+    <h4>Subhead with short description</h4>
+    <p>For those of you who don't know about Knoebels, they are the super fun amusement park down the road from us here in PA.  They are also home to some of the coolest rides around, including the #1 ranked Wooden Roller Coaster in the world - the Phoenix.  Rising from the ashes of its history as the Rocket roller coaster built in 1947 in San Antonio Texas, the Phoenix was reborn at Knoebels in 1985. In a collaboration with Knoebels, we built 4 snowboards out of actualy wooden planks from the orignal roller coaster!  Check out the journey here!</p>
+    </div>
+    </div>
+<?php include 'button-set.php' ?>
+    </div>
+    </div>
+    </div>
+<?php include 'footer.php' ?>
--- cool-stuff.php
+++ cool-stuff.php
@@ -1,12 +1,15 @@
 <?php include 'head.php' ?>
 <?php include 'header.php' ?>
-  <div class="main-content secondary">
-  	<div class="hero about">
+  <div class="main-content secondary cool-stuff-all">
+  	<div class="hero cool-stuff">
   		<h1>COOL STUFF</h1>
   	</div>
   <div class="content-wrapper">
   	<div class="inner">
   		<div class="full-width">
+  		<div class="intro-text">
+  		<h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h5>
+  		</div>
   		<div class="cool-stuff-wrapper">
   			<ul>
   			<li><a href=""><img src="images/cool-stuff/thumbs/tedtalk-thumb.png"><h6>Gilson Startup Story, told by Nick Gilson at TEDx 2017</h6></a></li>
