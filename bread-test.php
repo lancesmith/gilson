a45715036cd6257ed31898e4c6ca2a05af2a35da
Lance <lance.smith@echoorigin.com>
2019-01-16 14:13:15 -0500

    css and page updates for bread

---
+++ bread-test.php
@@ -0,0 +1,54 @@
+<?php include 'head.php' ?>
+<?php include 'header.php' ?>
+  <div class="main-content secondary">
+  	<!--<div class="hero apparel">
+  		<h1>FINANCING</h1>
+  	</div>-->
+<!--  <div class="content-wrapper">-->
+  	<div class="inner">
+  		<div class="full-width content-wrapper bread">
+  			<div class="bread-header">
+  		    <h1>Pay over time for as low as 0% APR*</h1>
+  		    <h3>Clear, transparent terms. Fair rates. No prepayment penalties.</h3>
+  		    <button class="add-cart">GET MY RATE</button>  	
+  			</div>	
+   		  <div class="bread-details">
+  		    <h2>0% APR Financing Available*</h2>
+  		    <p>We provide easy and affordable financing through our partner, <strong>Bread</strong>, so you can pay for your purchase over time.</p>
+  		      <ul>
+  			  	<li>
+  			  	<i class="far fa-calendar-alt"></i>
+  		        <h3>Affordable Monthly Plans</h3>
+  		        <p>Pay for your order over 3, 6, or 12 months at competitive interest rates as low as 0% APR*.</p>
+  		      </li>
+  		      <li>
+  		        <i class="far fa-thumbs-up"></i>
+  		        <h3>No Prepayment Penalties</h3>
+  		        <p>Pay for your purchase with monthly payments and prepay at any time without penalty.</p>
+  		      </li>
+  		      <li>
+  		        <i class="far fa-clock"></i>
+  		        <h3>Quick and Easy Application</h3>
+  		        <p>Get a decision in seconds with no obligation to buy. Checking your rate won&#39;t affect your credit score.</p>
+  			  </li>
+  		      </ul>
+  		  </div>
+  		
+  		  <div class="check-rate">
+  		    <h3>Check your rate in seconds without leaving our site</h3>
+  		    <button class="add-cart">GET MY RATE</button>  	
+  		  </div>
+  		
+  		  <div class="questions">
+  		    <h2>Questions?</h2>
+  		    <h4>Check Bread&#39;s <a href="https://www.getbread.com/faq/" target="_blank">FAQ</a> or reach Bread directly at <br><span><a href="mailto:support@getbread.com" target="_blank">support@getbread.com</a></span> or <span>(844) 992-7323 ext. 1</span>.</h4>
+  		    <p class="fine-print">*The terms are for a loan to finance a purchase. Rates range from 0% to 29.99% APR, resulting in, for example, 6 equal monthly payments of $166.67 at 0% APR, or  12 equal monthly payments of $86.52 to $97.48 at 6.99% to 29.99% APR, per $1,000 borrowed. Your terms may vary and are subject to credit approval.</p>
+  		    <p class="fine-print">Bread® loans are made by Cross River Bank, a New Jersey Chartered Bank, Member FDIC.</p>
+  		  </div>
+  		</div>
+  		</div>
+  		</div>
+<?php include 'button-set.php' ?>
+<!--    </div>-->
+    </div>
+<?php include 'footer.php' ?>
